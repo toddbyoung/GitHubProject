72a6c94dbc0c46751ea6c0b645eef5ce0d26dd4d
toddbyoung <todd.brian.young@gmail.com>
2024-12-01 10:51:31 -0600

    First Commit

---
+++ src/main/java/csci/githubproject/GitHubProject.java
@@ -0,0 +1,13 @@
+
+package csci.githubproject;
+
+/**
+ *
+ * @author Professor Todd Young
+ */
+public class GitHubProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
